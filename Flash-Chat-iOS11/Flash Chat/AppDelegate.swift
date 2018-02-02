6e25c43c8ed2afb0fb136a6fb6aa1760aef5d3aa
clurdablur <coucc15@gmail.com>
2018-02-02 16:08:50 -0600

    Chat app

--- Flash-Chat-iOS11/Flash Chat/AppDelegate.swift
+++ Flash-Chat-iOS11/Flash Chat/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 
 @UIApplicationMain
@@ -19,13 +20,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         //TODO: Initialise and Configure your Firebase here:
         
-        
+        FirebaseApp.configure()
         return true
     }
-
-    
-    
-    
     
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
