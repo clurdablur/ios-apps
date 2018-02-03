6df09d780fc3ee2911b8d8903089af9356d55e19
clurdablur <coucc15@gmail.com>
2018-02-03 07:53:20 -0600

    Items check and uncheck on todo list

---
+++ Honeydo/Honeydo/HoneydoViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Honeydo
+//
+//  Created by Claire Coulter on 2/2/18.
+//  Copyright © 2018 Claire Coulter. All rights reserved.
+//
+
+import UIKit
+
+class HoneydoViewController: UITableViewController {
+
+    let itemArray = ["Refinish Front Door", "Buy milk", "Fix backyard fence"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HoneydoCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+    
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
